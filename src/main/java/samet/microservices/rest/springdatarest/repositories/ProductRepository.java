29822e768bb1744a83c65b3573602457df38f1f9
asam-1 <bensam201201@gmail.com>
2023-05-08 15:27:31 +0200

    product microservice using spring data rest

--- src/main/java/samet/microservices/rest/springdatarest/repositories/ProductRepository.java
+++ src/main/java/samet/microservices/rest/springdatarest/repositories/ProductRepository.java
@@ -1,7 +1,9 @@
 package samet.microservices.rest.springdatarest.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import samet.microservices.rest.springdatarest.model.Product;
 
+@RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
