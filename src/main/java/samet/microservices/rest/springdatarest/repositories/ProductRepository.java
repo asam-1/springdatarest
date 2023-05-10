b0c9cd48418cc52b963064bd8fc7352f7e5d133c
asam-1 <bensam201201@gmail.com>
2023-05-10 07:37:52 +0200

    adding search method to the repository

--- src/main/java/samet/microservices/rest/springdatarest/repositories/ProductRepository.java
+++ src/main/java/samet/microservices/rest/springdatarest/repositories/ProductRepository.java
@@ -1,9 +1,12 @@
 package samet.microservices.rest.springdatarest.repositories;
 
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import samet.microservices.rest.springdatarest.model.Product;
 
 @RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByNameContains(String name, SpringDataWebProperties.Pageable pageable);
 }
